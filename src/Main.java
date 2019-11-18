b4eff960621b55632a36111bdf854c9d65f89a5c
Tod <961832379@qq.com>
2019-11-18 13:43:49 +0800

    MongoDB创建及获取集合

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 /**
  * @Author: Tod
@@ -9,8 +11,21 @@ import com.mongodb.client.MongoDatabase;
  */
 public class Main {
     public static void main(String[] args) {
-        MongoClient mongoClient = new MongoClient("localhost",27017);
-        MongoDatabase test = mongoClient.getDatabase("test");
-        System.out.println("Connect to database successfully!");
+
+        try {
+            // 建立连接
+            MongoClient mongoClient = new MongoClient("localhost",27017);
+            // 获取数据库
+            MongoDatabase test = mongoClient.getDatabase("test");
+            System.out.println("Connect to database successfully!");
+//            // 创建集合
+//            test.createCollection("mycoll");
+//            System.out.println("创建集合成功");
+            // 获取集合
+            MongoCollection<Document> mycoll = test.getCollection("mycoll");
+            System.out.println("获取集合mycoll成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
